1ef113b917b2cca4e6cffe71f8b0275cbb1a98b5
Ivan Schuetz <ivanschuetz@gmail.com>
2020-04-20 23:28:47 +0200

    Wiring, refactor

--- CoEpi/Dependencies.swift
+++ CoEpi/Dependencies.swift
@@ -23,7 +23,7 @@ class Dependencies {
     }
 
     private func registerViewModels(container: DependencyContainer) {
-        container.register { HomeViewModel() }
+        container.register { HomeViewModel(startPermissions: try container.resolve()) }
         container.register { OnboardingWireframe(container: container) }
         container.register { OnboardingViewModel() }
         container.register { HealthQuizViewModel(symptomRepo: try container.resolve()) }
@@ -63,7 +63,7 @@ class Dependencies {
 
     private func registerLogic(container: DependencyContainer) {
         container.register(.singleton) { CenLogic() }
-        container.register(.eagerSingleton) {ShowNotifications(CoEpiRepo: try container.resolve())}
+        container.register(.eagerSingleton) { AlertNotificationsShower(alertsRepo: try container.resolve()) }
     }
 
     private func registerNetworking(container: DependencyContainer) {
@@ -84,6 +84,7 @@ class Dependencies {
 
     private func registerSystem(container: DependencyContainer) {
         container.register(.singleton) { KeyValueStoreImpl() as KeyValueStore }
+        container.register(.singleton) { StartPermissionsImpl() as StartPermissions }
     }
 }
 
