c1b1b74956100b4015f58a4190e4f758e2bc0f39
Stewart, Allen <Allen.Stewart@gtri.gatech.edu>
2020-04-21 13:24:52 -0400

    Update AlertNotificationsShower.swift

--- CoEpi/ui/alerts/AlertNotificationsShower.swift
+++ CoEpi/ui/alerts/AlertNotificationsShower.swift
@@ -40,14 +40,17 @@ class AlertNotificationsShower {
     }
 
     private func showNewAlertsNotification() {
-        os_log("Showing alerts notification", log: servicesLog, type: .debug)
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if error == nil {
+                os_log("Showing alerts notification", log: servicesLog, type: .debug)
 
-        let content = UNMutableNotificationContent()
-        content.title = "New Contact Alerts"
-        content.body = "New contact alerts have been detected. Tap for details."
-        content.sound = .default
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content,
-                                            trigger: UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false))
-        UNUserNotificationCenter.current().add(request)
+                       let content = UNMutableNotificationContent()
+                       content.title = "New Contact Alerts"
+                       content.body = "New contact alerts have been detected. Tap for details."
+                       content.sound = .default
+                       let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false))
+                       UNUserNotificationCenter.current().add(request)
+            }
+        }
     }
 }
