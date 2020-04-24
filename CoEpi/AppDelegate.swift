b0f833a4666dd55584b9b0a134de206761383495
Ivan Schuetz <ivanschuetz@gmail.com>
2020-04-24 13:26:40 +0200

    Clear badge coming to FG, add scene delegate

--- CoEpi/AppDelegate.swift
+++ CoEpi/AppDelegate.swift
@@ -6,14 +6,13 @@ import AppCenterDistribute
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    private let container: DependencyContainer = Dependencies().createContainer()
+    let container: DependencyContainer = Dependencies().createContainer()
 
     internal var window: UIWindow?
     
     private var rootWireframe: RootWireFrame?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        initUI()
         MSAppCenter.start("2581ac81-eb5b-423a-83e6-65cab9d64dd8", withServices: [MSDistribute.self])
 
         let bgManager: BackgroundTasksManager = try! container.resolve()
@@ -25,6 +24,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         let bgManager: BackgroundTasksManager = try! container.resolve()
         bgManager.applicationDidEnterBackground()
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
     // MARK: - Core Data stack
@@ -70,13 +83,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
-    }
-
-    // MARK: - Private
-
-    private func initUI() {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        rootWireframe = RootWireFrame(container: container, window: window)
-        self.window = window
     }
 }
